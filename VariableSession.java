71437b25487ccc0344f5606d4e3e8c952c8b0667
sinchanasc <sinchanasc.xworkz@gmail.com>
2023-06-20 16:15:15 +0530

    VariableSession-push

---
+++ VariableSession.java
@@ -0,0 +1,12 @@
+public class VariableSession {
+
+public static void main(String []args){
+
+int priceOfMobile = 1000;
+System.out.println(priceOfMobile);
+
+priceOfMobile = 15000;
+system.out.println(priceOfMobile);
+}
+}
+
