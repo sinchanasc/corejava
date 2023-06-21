01b881e712f741a34f9c095daa9c593aff34462c
sinchanasc <sinchanasc.xworkz@gmail.com>
2023-06-21 09:00:38 +0530

    ArrayClass-push

--- ArrayClass.java
+++ ArrayClass.java
@@ -9,7 +9,7 @@ public class ArraySession{
 	int[] noOfSchools = {1,2,3,4,5};
 	System.out.println(noOfSchools[3]);
 	
-	int[] priceOfMobiles = {10000,15000,20000,25000,30000};
+	int[] priceOfMobiles = {10000,15000,17000,19000,20000};
 	System.out.println(priceOfMobiles[3]);
 	
 	char[] vowels = {'a','e','i','o','u'};
@@ -19,7 +19,7 @@ public class ArraySession{
 	System.out.println(names[3]);
 	
 	char[] divisions = {'A','B','C','D','E'};
-	System.out.println(divisions[3]);
+	System.out.println(names[3]);
 	
 	float[] cgpa = {8.1f,8.4f,7.5f,9.1f,7.0f};
 	System.out.println(cgpa[3]);
@@ -30,13 +30,13 @@ public class ArraySession{
 	float[] weigth = {62.8f,70.8f,30.2f,25.6f,40.6f};
 	System.out.println(weigth[3]);
 	
+	
 	int[] noOfFloors = new int[5];
 	noOfFloors[0] = 1;
 	noOfFloors[1] = 2;
 	noOfFloors[2] = 3;
 	noOfFloors[3] = 4;
 	noOfFloors[4] = 5;
-	
 	int[] ramOfMobiles = new int[5];
 	ramOfMobiles[0] = 8;
 	ramOfMobiles[1] = 16;
@@ -81,21 +81,18 @@ public class ArraySession{
 	age[2] = 11.5f;
 	age[3] = 1.5f;
 	age[4] = 17.5f;
-	
 	float[] temperature = new float[5];
 	temperature[0] = 32.8f;
 	temperature[1] = 30.3f;
 	temperature[2] = 33.5f;
 	temperature[3] = 35.8f;
 	temperature[4] = 31.2f;
-	
 	float[] fruitsWeigth = new float[5];
 	fruitsWeigth[0] = 20.5f;
 	fruitsWeigth[1] = 10.5f;
 	fruitsWeigth[2] = 11.5f;
 	fruitsWeigth[3] = 15.5f;
 	fruitsWeigth[4] = 22.8f;
-	
 	for(int i=0;i<noOfFloors.length;i++){
 		System.out.println(noOfFloors[i]);
 	}
