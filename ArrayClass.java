d0922a7e616a6b4680a14dd0a8401cc032b9d405
sinchanasc <sinchanasc.xworkz@gmail.com>
2023-07-04 20:00:23 +0530

    array-push

---
+++ ArrayClass.java
@@ -0,0 +1,140 @@
+public class ArrayClass{
+	public static void main(String[] args){
+	
+	int[] noOfStudentes = {10,20,30,40,50};
+	System.out.println(noOfStudentes[3]);
+	
+	int[] noOfBooks = {5,6,7,8,9};
+	System.out.println(noOfBooks[3]);
+	
+	int[] noOfSchools = {1,2,3,4,5};
+	System.out.println(noOfSchools[3]);
+	
+	int[] priceOfMobiles = {10000,15000,17000,19000,20000};
+	System.out.println(priceOfMobiles[3]);
+	
+	char[] vowels = {'a','e','i','o','u'};
+	System.out.println(vowels[3]);
+	
+	char[] names = {'h','a','r','s','h'};
+	System.out.println(names[3]);
+	
+	char[] divisions = {'A','B','C','D','E'};
+	System.out.println(names[3]);
+	
+	float[] cgpa = {8.1f,8.4f,7.5f,9.1f,7.0f};
+	System.out.println(cgpa[3]);
+	
+	float[] tenthPercentage = {80.33f,88.38f,90.0f,70.32f,69.6f};
+	System.out.println(tenthPercentage[3]);
+	
+	float[] weight = {62.8f,70.8f,30.2f,25.6f,40.6f};
+	System.out.println(weigth[3]);
+	
+	
+	int[] noOfFloors = new int[5];
+	noOfFloors[0] = 1;
+	noOfFloors[1] = 2;
+	noOfFloors[2] = 3;
+	noOfFloors[3] = 4;
+	noOfFloors[4] = 5;
+	
+	int[] ramOfMobiles = new int[5];
+	ramOfMobiles[0] = 8;
+	ramOfMobiles[1] = 16;
+	ramOfMobiles[2] = 24;
+	ramOfMobiles[3] = 32;
+	ramOfMobiles[4] = 64;
+	
+	int[] lengthOfPipes = new int[5];
+	lengthOfPipes[0] = 10;
+	lengthOfPipes[1] = 20;
+	lengthOfPipes[2] = 30;
+	lengthOfPipes[3] = 40;
+	lengthOfPipes[4] = 50;
+	
+	int[] noOfFemaleCandidates = new int[5];
+	noOfFemaleCandidates[0] = 1;
+	noOfFemaleCandidates[1] = 2;
+	noOfFemaleCandidates[2] = 3;
+	noOfFemaleCandidates[3] = 4;
+	noOfFemaleCandidates[4] = 5;
+	
+	char[] rainbowColors = new char[7];
+	rainbowColors[0] = 'r';
+	rainbowColors[1] = 'o';
+	rainbowColors[2] = 'y';
+	rainbowColors[3] = 'g';
+	rainbowColors[4] = 'b';
+	rainbowColors[5] = 'i';
+	rainbowColors[6] = 'v';
+	
+	char[] subjects = new char[5];
+	subjects[0] = 'e';
+	subjects[1] = 'k';
+	subjects[2] = 'h';
+	subjects[3] = 'm';
+	subjects[4] = 's';
+	
+	char[] colorOfTree = new char[5];
+	colorOfTree[0] = 'g';
+	colorOfTree[1] = 'r';
+	colorOfTree[2] = 'e';
+	colorOfTree[3] = 'e';
+	colorOfTree[4] = 'n';
+	
+	float[] age = new float[5];
+	age[0] = 10.5f;
+	age[1] = 12.5f;
+	age[2] = 11.5f;
+	age[3] = 1.5f;
+	age[4] = 17.5f;
+	
+	float[] temperature = new float[5];
+	temperature[0] = 32.8f;
+	temperature[1] = 30.3f;
+	temperature[2] = 33.5f;
+	temperature[3] = 35.8f;
+	temperature[4] = 31.2f;
+	
+	float[] fruitsWeigth = new float[5];
+	fruitsWeigth[0] = 20.5f;
+	fruitsWeigth[1] = 10.5f;
+	fruitsWeigth[2] = 11.5f;
+	fruitsWeigth[3] = 15.5f;
+	fruitsWeigth[4] = 22.8f;
+	
+	for(int i=0;i<noOfFloors.length;i++){
+		System.out.println(noOfFloors[i]);
+	}
+	for(int i=0;i<ramOfMobiles.length;i++){
+		System.out.println(ramOfMobiles[i]);
+	}
+	for(int i=0;i<temperature.length;i++){
+	System.out.println(temperature[i]);
+	}
+	for(int i=0;i<fruitsWeigth.length;i++){
+	System.out.println(fruitsWeigth[i]);
+	}
+	for(int i=0;i<colorOfTree.length;i++){
+	System.out.println(colorOfTree);
+	}
+	System.out.println(lengthOfPipes);
+	System.out.println(noOfFemaleCandidates);
+	System.out.println(rainbowColors);
+	System.out.println(subjects);
+	System.out.println(age);
+	
+	
+    	
+	
+	
+	
+	
+	}
+	
+
+
+
+
+}
